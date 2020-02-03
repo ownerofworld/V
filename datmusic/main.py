e470faeb2a3e87d365b018d22c0767ad7d7a8167
ownerofworld <43425662+ownerofworld@users.noreply.github.com>
2020-02-04 02:12:33 +0530

    Update main.py

--- datmusic/main.py
+++ datmusic/main.py
@@ -45,7 +45,7 @@ def inlinequery(bot, update):
                     )
                 )
             update.inline_query.answer(results=results, cache_time=INLINE_QUERY_CACHE_TIME)
-    except Exception, e:
+    except Exception as e:
         error(bot, update, e)
 
 def search(query):
